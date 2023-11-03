ea9c62580b9b9f591dafc4a81de35d8ad1cae11d
Vadim <vv3975017@gmail.com>
2023-11-04 00:20:46 +0500

    комит

---
+++ flask_blog/config.py
@@ -0,0 +1,10 @@
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "letavad777@yandex.ru"
+    MAIL_PASSWORD = "otfkeywadsncmcgj"
