698c861e2267d35b030126f7dd9f7dbfc16ca08f
VadimVasinAndreevich <132100792+VadimVasinAndreevich@users.noreply.github.com>
2023-11-05 15:12:40 +0500

    Update config.py

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -5,6 +5,7 @@ class Config:
 
     MAIL_SERVER = 'smtp.yandex.ru'
     MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = "letavad777@yandex.ru"
     MAIL_PASSWORD = "otfkeywadsncmcgj"
